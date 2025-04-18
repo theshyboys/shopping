ae6739beb557c02ae94f71e63f9f9e28ce941773
TEERA YOOSUK <theshyboys@gmail.com>
2025-04-18 09:51:30 +0700

    first commit

--- src/app/downloadimg/page.js
+++ src/app/downloadimg/page.js
@@ -20,6 +20,32 @@ const downloadImage = async () => {
     URL.revokeObjectURL(url);
   };
   
+
+    const handleDownload = () => {
+      const img = new Image();
+      img.src = '/product/grp05-2/content.png';
+      img.crossOrigin = 'Anonymous';
+      
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+        
+        canvas.toBlob((blob) => {
+          const url = URL.createObjectURL(blob);
+          const link = document.createElement('a');
+          link.href = url;
+          link.download = 'nextjs-image.jpg';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+          URL.revokeObjectURL(url);
+        }, 'image/jpeg', 0.9);
+      };
+    };
   
   export default function DownloadButton() {
 
@@ -27,7 +53,7 @@ const downloadImage = async () => {
   useEffect(() => {
     const timer = setTimeout(() => {
         // เปลี่ยนไปยังหน้าหลัก (home) หลังจาก 3 วินาที
-        downloadImage();
+        //downloadImage();
       }, 1000);
 
       // ล้างการตั้งเวลาเมื่อคอมโพเนนต์ถูกถอด
@@ -38,7 +64,7 @@ const downloadImage = async () => {
 
     return (
       <button
-        onClick={downloadImage}
+        onClick={handleDownload}
         className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
       >
         Save Image
