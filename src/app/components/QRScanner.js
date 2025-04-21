57607fced321b69fd9ddfad3cdd40fd4cfc112cc
TEERA YOOSUK <theshyboys@gmail.com>
2025-04-21 11:43:47 +0700

    first commit

--- src/app/components/QRScanner.js
+++ src/app/components/QRScanner.js
@@ -37,7 +37,7 @@ export default function QRScanner() {
 
         const config = {
           fps: 10,
-          qrbox: { width: 200, height: 200 },
+          //qrbox: { width: 200, height: 200 },
           //rememberLastUsedCamera: true,
           //supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
         };
